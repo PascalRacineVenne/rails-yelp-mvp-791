cfb84ed132259389e4d9a07e669c9cf6f95ce219
Pascal Racine-Venne <pascalracinevenne@gmail.com>
2022-02-16 01:11:07 -0500

    logic all happening need to refactor

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,22 +1,24 @@
 class ReviewsController < ApplicationController
-  # def new
-  #   @review = Review.new
-  # end
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
 
   def create
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new(review_params)
     @review.restaurant = @restaurant
+
     if @review.save
       redirect_to restaurant_path(@restaurant)
    else
-      render 'restaurant/show'
+     render :new
    end
   end
 
   private
 
   def review_params
-    params.require(:review).permit(:content, :rating)
+    params.require(:review).permit(:rating, :content)
   end
 end
