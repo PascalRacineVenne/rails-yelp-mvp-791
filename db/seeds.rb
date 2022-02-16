487172a67122edb3bee9c346c2bee4077250724f
Pascal Racine-Venne <pascalracinevenne@gmail.com>
2022-02-15 22:41:29 -0500

    created seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "cleaning up databse"
+Restaurant.destroy_all
+puts "database is clean"
+
+puts "creating restaurants"
+5.times do
+  restaurant = Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+  )
+  puts "restaurant #{restaurant.id} is created"
+end
+
+puts 'done'
